82a1881fd65b7a6eacd252d5d982dc85cc7d97ad
Denis(mac) <denistesla2005@gmail.com>
2024-03-12 00:01:44 +0200

    mock data gen

--- shop-back.Server/Services/GenerateMockData.cs
+++ shop-back.Server/Services/GenerateMockData.cs
@@ -7,6 +7,7 @@
 
 using shop_back.Server.Data;
 using shop_back.Server.Entities;
+using shop_back.Server.Models;
 
 namespace shop_back.Server.Services;
 // For Entity Framework
@@ -32,107 +33,81 @@ public static class GenerateMockData
             {
                 Email = "test2@test.com",
                 UserName = "test2"
-            });
-        context.SaveChanges();
+            },
+            new Microsoft.AspNetCore.Identity.IdentityUser
+            {
+                Email = "test3@test.com",
+                UserName = "test3"
+            },
+            new Microsoft.AspNetCore.Identity.IdentityUser
+            {
+                Email = "test4@test.com",
+                UserName = "test4"
+            }
 
+        );
+        context.SaveChanges();
+        MakeProduct(context, true, true);
+        MakeProduct(context, true, false);
+        MakeProduct(context, false, true);
+        MakeProduct(context, false, false);
+
+        for (int i = 0; i < 20; i++)
+        {
+            MakeProduct(context, false, Random.Shared.Next(0, 2) == 1);
+        }
+    }
+
+    public static void MakeProduct(MainContext ctx, bool inclOrder = false, bool inclFeedback = false){
+        string[] names = {"MacBook", "iPhone", "iPad", "iMac", "Apple Watch", "AirPods", "HomePod", "Apple TV"};
+        string[] categories = {"Laptops", "Smartphones", "Tablets", "Desktops", "Watches", "Headphones", "Speakers", "TVs"};
+        string[] images = {"macbook.jpg", "iphone.jpg", "ipad.jpg", "imac.jpg", "applewatch.jpg", "airpods.jpg", "homepod.jpg", "appletv.jpg"};
+        string[] characteristics = {"Retina display", "Touch ID", "Face ID", "Apple M1 chip", "Always-on Retina display", "Active Noise Cancellation", "Spatial audio", "Dolby Atmos"};
         var product = new ProductEntity
         {
-            Name = "TestProduct",
-            Characteristics = new List<string> { "TestCharacteristic" },
-            Category = "TestCategory",
-            Discount = 0,
-            Images = new List<string> { "TestImage" },
-            IsHit = false,
-            IsAvailable = true,
-            Price = 100,
-            Quantity = 10
+            Name = names[Random.Shared.Next(0, names.Length)],
+            Characteristics = new List<string> { characteristics[Random.Shared.Next(0, characteristics.Length)] },
+            Category = categories[Random.Shared.Next(0, categories.Length)],
+            Discount = Random.Shared.Next(0, 100),
+            Images = new List<string> { images[Random.Shared.Next(0, images.Length)] },
+            IsHit = Random.Shared.Next(0, 2) == 1,
+            IsAvailable = Random.Shared.Next(0, 10) > 0,
+            Price = Random.Shared.Next(100, 10000),
+            Quantity = Random.Shared.Next(0, 100)
         };
-        context.Products.Add(product);
-        context.SaveChanges();
-
-        var feedback = new FeedbackEntity
+        ctx.Products.Add(product);
+        ctx.SaveChanges();
+        if (inclFeedback)
         {
-            Product = product,
-            Text = "TestFeedback",
-            UserId = "1",
-            Rating = 5,
-            ProductId = product.Id
-        };
-        context.Feedbacks.Add(feedback);
-        context.SaveChanges();
-
-        var order = new OrderEntity
+            var rngActiveUsr = ctx.Users.ToArray()[Random.Shared.Next(0, ctx.Users.Count())];
+            var feedbackM = new FeedbackModel
+            {
+                Text = characteristics[Random.Shared.Next(0, characteristics.Length)] + " is an awesome feature of " + product.Name,
+                Rating = Random.Shared.Next(1, 6),
+                ProductId = product.Id
+            };
+            var feedback = new FeedbackEntity(feedbackM, product, rngActiveUsr.Id);
+            ctx.Feedbacks.Add(feedback);
+            ctx.SaveChanges();
+        }
+        if (inclOrder)
         {
-            Product = product,
-            Quantity = 1,
-            PriceOrdered = 100,
-            DateOrdered = DateTime.Now,
-            UserId = "1",
-            LegalName = "Alice",
-            PhoneNumber = "+1 234 567 890",
-            City = "Kyiv",
-            PostOffice = "1",
-            DeliveryMethod = "Nova Poshta",
-            PaymentMethod = "Cash"
-        };
-        context.Orders.Add(order);
-        context.SaveChanges();
-
-        var product2 = new ProductEntity
-        {
-            Name = "TestProduct2",
-            Characteristics = new List<string> { "TestCharacteristic2" },
-            Category = "TestCategory2",
-            Discount = 0,
-            Images = new List<string> { "TestImage2" },
-            IsHit = false,
-            IsAvailable = true,
-            Price = 100,
-            Quantity = 10
-        };
-        context.Products.Add(product2);
-        context.SaveChanges();
-
-        var feedback2 = new FeedbackEntity
-        {
-            Product = product2,
-            Text = "TestFeedback2",
-            UserId = "1"
-        };
-        context.Feedbacks.Add(feedback2);
-        context.SaveChanges();
-
-        var order2 = new OrderEntity
-        {
-            Product = product2,
-            Quantity = 1,
-            PriceOrdered = 100,
-            DateOrdered = DateTime.Now,
-            UserId = "1",
-            LegalName = "Alice",
-            PhoneNumber = "+1 234 567 890",
-            City = "Kyiv",
-            PostOffice = "1",
-            DeliveryMethod = "Nova Poshta",
-            PaymentMethod = "Cash"
-        };
-        context.Orders.Add(order2);
-        context.SaveChanges();
-
-        var product3 = new ProductEntity
-        {
-            Name = "TestProduct3",
-            Characteristics = new List<string> { "TestCharacteristic3" },
-            Category = "TestCategory3",
-            Discount = 0,
-            Images = new List<string> { "TestImage3" },
-            IsHit = false,
-            IsAvailable = true,
-            Price = 100,
-            Quantity = 10
-        };
-        context.Products.Add(product3);
-        context.SaveChanges();
-
+            var order = new OrderEntity
+            {
+                Product = product,
+                Quantity = Random.Shared.Next(1, 10),
+                PriceOrdered = product.Price * Random.Shared.Next(1, 10),
+                DateOrdered = DateTime.Now,
+                UserId = "1",
+                LegalName = "Alice",
+                PhoneNumber = "+1 234 567 890",
+                City = "Kyiv",
+                PostOffice = "1",
+                DeliveryMethod = "Nova Poshta",
+                PaymentMethod = "Cash"
+            };
+            ctx.Orders.Add(order);
+            ctx.SaveChanges();
+        }
     }
 }
