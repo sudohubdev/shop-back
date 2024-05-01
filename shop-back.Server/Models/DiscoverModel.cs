c75ae87d6c3454849b28da2522159b47455b3899
Denis (PC) <denistesla2005@gmail.com>
2024-05-01 10:17:44 +0300

    update discover

--- shop-back.Server/Models/DiscoverModel.cs
+++ shop-back.Server/Models/DiscoverModel.cs
@@ -5,10 +5,10 @@ namespace MvcProxy.Models
         public string? Name { get; set; } = "Shop Labwork";
         public string? Url { get; set; } = "shop.sudohub.dev";
         public string? Img { get; set; } = "https://pickystory.com/wp-content/uploads/2022/08/advantages-of-ecommerce-shopping-cart.png";
-        public bool Auth { get; set; } = true;
+        public bool Auth { get; set; } = false;
         public bool Online { get; set; } = true;
         //required specifically for direct forwarding
-        public string? Ip { get; set; } = "192.168.0.102";
+        public string? Ip { get; set; } = "192.168.0.208";
         public int Port { get; set; } = 8008;
         public string Path { get; set; } = "";
         public bool Direct { get; set; } = true;
